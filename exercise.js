e1143a7cf543ee079a3c3b69de0f9bbe1535cde9
LinaYahya <linaebe0@gmail.com>
2021-07-02 22:57:04 +0300

    add extra exercises

--- exercise.js
+++ exercise.js
@@ -1,39 +1,60 @@
-// 1 - Write a function which, if the 'h2' element  has the class "header",
-// it will be removed; otherwise the class "toogleHeader" is added.
-
+/*
+Write a function which, if the 'h2' element  has the class "header",
+it will be removed; otherwise the class "header" is added.
+*/
 const toggleClass = () => {};
 // toggleClass(); //uncomment me to test
 
-// 2- Select the section with an id of 'container' without using querySelector,
-// and style the font with 'blue' color
+/*
+Select the section with an id of 'container' without using querySelector,
+and style the font with 'blue' color
+*/
 
 const styleContainer = () => {};
 // styleContainer(); //uncomment me to test
 
-// 3 - Select the 'nav' with a class of "firstNav" and style the font with 'italic' fontStyle.
+// Select the 'nav' with a class of "firstNav" and style the font with 'italic' fontStyle.
 
 const styleItalic = () => {};
 // styleItalic(); //uncomment me to test
 
-// 4 - Select a list item with a class of 'contact', but only the list item inside of the 'secondList' nav,
-// and style it with background color 'yellow'
+/*
+Select a list item with a class of 'contact', but only the list item inside of the 'secondList' nav,
+and style it with background color 'yellow'
+*/
 
 const backgroundList = () => {};
-
 // backgroundList(); //uncomment me to test
 
-// 5- Create a new 'li' element , give it the text 'project' and append it to the 'fisrtList' .
+// Create a new 'li' element , give it the text 'project' and append it to the 'firstList' .
 const newList = () => {};
-newList(); //uncomment me to test
+// newList(); //uncomment me to test
 
-// 6 - Loop over all of the 'li' elements inside the 'secondList' and give them a background color of "brown".
+// Loop over all of the 'li' elements inside the 'secondList' and give them a background color of "brown".
 const brownColor = () => {};
 // brownColor(); //uncomment me to test
 
-// 7- Create 'p' eleement with any text and append it to the footer
-const addFooter = () => {};
-// addFooter(); //uncomment me to test
+/*
+Write a function which returns the value in the `firstName` text input
+*/
+const getFirstNameValue = () => {};
+// getFirstNameValue(); //uncomment me to test
 
-// 8- Remove the 'div' with a class of 'footer'.
+/*
+Write a function which takes a string, `inputName`, and returns the value in the input which
+has `inputName` as the name attribute
+*/
+const getValue = (inputName) => {};
+// getValue(put an argument here ); //uncomment me to test
+
+/*
+Write a function, `generateUl`, which takes an array of strings, and returns a
+`ul` htmlElement containing `li` elements for each array element. Each `li'
+should contain the value of the array element.
+*/
+const generateUl = (array) => {};
+// generateUl(put an argument here ); //uncomment me to test
+
+// Remove the 'div' with a class of 'footer'.
 const removeFooter = () => {};
 // removeFooter(); //uncomment me to test
